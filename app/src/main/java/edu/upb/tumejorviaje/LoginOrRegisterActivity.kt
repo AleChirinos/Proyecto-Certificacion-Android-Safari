d4a71e0232d52b48284b0d5e009b168a64930db0
element222 <fabian.machicao@gmail.com>
2021-09-30 20:24:34 -0400

    Register Activity

--- app/src/main/java/edu/upb/tumejorviaje/LoginOrRegisterActivity.kt
+++ app/src/main/java/edu/upb/tumejorviaje/LoginOrRegisterActivity.kt
@@ -1,11 +1,22 @@
 package edu.upb.tumejorviaje
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginOrRegisterActivity : AppCompatActivity() {
+    lateinit var registerButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_or_register)
+        registerButton = findViewById(R.id.RegisterButton1)
+        registerButton.setOnClickListener {
+            val intent : Intent=Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
