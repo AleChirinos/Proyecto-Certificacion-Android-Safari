28dcad08e08bbb514165cdd40ba1d7e754919868
nickygon <nikita27111@hotmail.com>
2021-10-03 08:51:21 -0400

    Commit de arreglos del activity de direct chat

--- app/src/main/java/edu/upb/tumejorviaje/data/ChatDirectTempDataSource.kt
+++ app/src/main/java/edu/upb/tumejorviaje/data/ChatDirectTempDataSource.kt
@@ -4,7 +4,7 @@ import edu.upb.tumejorviaje.model.ChatBubble
 
 object ChatDirectTempDataSource {
     val chatBubbleList= mutableListOf(
-        ChatBubble("Esto ya no es divertido, es triste","9:50","https://cdn6.recetasdeescandalo.com/wp-content/uploads/2020/04/Chili-con-carne-muy-facil-y-con-mucho-sabor.jpg"),
+        ChatBubble("Esto ya no es divertido, es triste","9:50","https://i1.sndcdn.com/artworks-000151844204-04fr65-t500x500.jpg"),
         ChatBubble("Te pasaste de lanza con ese chiste","9:50","https://i1.sndcdn.com/artworks-000151844204-04fr65-t500x500.jpg"),
         ChatBubble("Por favor, para con esto","9:51","https://i1.sndcdn.com/artworks-000151844204-04fr65-t500x500.jpg"),
         ChatBubble("Lo siento, no lo volveré a hacer, es triste","9:50","https://i.dailymail.co.uk/1s/2019/01/23/09/8878274-6622497-image-a-23_1548237108686.jpg"),
