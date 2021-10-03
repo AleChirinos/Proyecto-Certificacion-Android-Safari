af8ecbdf74f9ad8028a53f6bfec3d83f4e01a9a4
element222 <fabian.machicao@gmail.com>
2021-10-02 23:33:42 -0400

    Chats fragment

---
+++ app/src/main/java/edu/upb/tumejorviaje/data/ChatsTempDataSource.kt
@@ -0,0 +1,24 @@
+package edu.upb.tumejorviaje.data
+
+import edu.upb.tumejorviaje.R
+import edu.upb.tumejorviaje.post.SavedChat
+
+object ChatsTempDataSource {
+    val chatsList = mutableListOf(
+        SavedChat(
+            chatName = "Maria Marcela",
+            chatLastMessage = "Aún estoy esperando.",
+            profileImg = R.drawable.ic_circle_profile_1
+        ),
+        SavedChat(
+            chatName = "Rodrigo Cárdenas",
+            chatLastMessage = "Espero funcione.",
+            profileImg = R.drawable.ic_circle_profile_2
+        ),
+        SavedChat(
+            chatName = "Daniel Delgado",
+            chatLastMessage = "Ya lo sabía",
+            profileImg = R.drawable.ic_circle_profile_3
+        )
+    )
+}
--- app/src/main/java/edu/upb/tumejorviaje/ui/fragments/ChatFragment.kt
+++ app/src/main/java/edu/upb/tumejorviaje/ui/fragments/ChatFragment.kt
@@ -5,14 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import edu.upb.tumejorviaje.R
+import edu.upb.tumejorviaje.data.ChatsTempDataSource
+import edu.upb.tumejorviaje.ui.adapters.ChatListAdapter
 
 class ChatFragment: Fragment(){
+    private val chatListAdapter = ChatListAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_chat, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.rvChats)
+        recyclerView.adapter = chatListAdapter
+
+        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        chatListAdapter.addAll(ChatsTempDataSource.chatsList)
     }
 }
