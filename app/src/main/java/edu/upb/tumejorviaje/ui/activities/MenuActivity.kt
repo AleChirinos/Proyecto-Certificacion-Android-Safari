87f2420bb0302ee8a7e6674c269494d7143bb60b
element222 <fabian.machicao@gmail.com>
2021-10-06 21:11:44 -0400

    Menu Navigation

--- app/src/main/java/edu/upb/tumejorviaje/ui/activities/MenuActivity.kt
+++ app/src/main/java/edu/upb/tumejorviaje/ui/activities/MenuActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.upb.tumejorviaje.R
 import edu.upb.tumejorviaje.ui.fragments.*
@@ -36,26 +38,31 @@ class MenuActivity : AppCompatActivity() {
 
         //Para navegar por los fragments
         menuNavigationView=findViewById(R.id.menuNavigationView)
-        menuNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.menuItemProfile->{
-                    replaceFragment(R.id.mainContainer,profileFragment)
-                }
-                R.id.menuItemChat->{
-                    replaceFragment(R.id.mainContainer,chatFragment)
-                }
-                R.id.menuItemFeed->{
-                    replaceFragment(R.id.mainContainer,feedFragment)
-                }
-                R.id.menuItemSearch->{
-                    replaceFragment(R.id.mainContainer,searchFragment)
-                }
-                R.id.menuItemUpload->{
-                    replaceFragment(R.id.mainContainer,uploadFragment)
-                }
-            }
-            true
-        }
-        menuNavigationView.selectedItemId = R.id.menuItemFeed
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        menuNavigationView.setupWithNavController(navController)
+//        menuNavigationView.setOnItemSelectedListener {
+//            when(it.itemId){
+//                R.id.menuItemProfile->{
+//                    replaceFragment(R.id.mainContainer,profileFragment)
+//                }
+//                R.id.menuItemChat->{
+//                    replaceFragment(R.id.mainContainer,chatFragment)
+//                }
+//                R.id.menuItemFeed->{
+//                    replaceFragment(R.id.mainContainer,feedFragment)
+//                }
+//                R.id.menuItemSearch->{
+//                    replaceFragment(R.id.mainContainer,searchFragment)
+//                }
+//                R.id.menuItemUpload->{
+//                    replaceFragment(R.id.mainContainer,uploadFragment)
+//                }
+//            }
+//            true
+//        }
+//        menuNavigationView.selectedItemId = R.id.menuItemFeed
     }
 }
