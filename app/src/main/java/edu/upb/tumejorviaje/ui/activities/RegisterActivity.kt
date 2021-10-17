4459fd087f977d8c081eefe83ea2b51f54c6870b
element222 <fabian.machicao@gmail.com>
2021-10-17 19:56:29 -0400

    Register View Binding

--- app/src/main/java/edu/upb/tumejorviaje/ui/activities/RegisterActivity.kt
+++ app/src/main/java/edu/upb/tumejorviaje/ui/activities/RegisterActivity.kt
@@ -1,12 +1,26 @@
 package edu.upb.tumejorviaje.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import edu.upb.tumejorviaje.R
+import edu.upb.tumejorviaje.databinding.ActivityLoginOrRegisterBinding
+import edu.upb.tumejorviaje.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.RegisterButton1.setOnClickListener{
+            val intent : Intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
