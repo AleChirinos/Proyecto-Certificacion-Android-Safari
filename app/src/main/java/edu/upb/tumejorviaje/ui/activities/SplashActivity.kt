ef5060b29ae5db8ffa902100335bdca4a81f22dc
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-10-17 16:49:59 -0400

    View binding on Splash Activity

--- app/src/main/java/edu/upb/tumejorviaje/ui/activities/SplashActivity.kt
+++ app/src/main/java/edu/upb/tumejorviaje/ui/activities/SplashActivity.kt
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import edu.upb.tumejorviaje.R
+import edu.upb.tumejorviaje.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Handler().postDelayed({
             val intent = Intent(this, LoginOrRegisterActivity::class.java)
