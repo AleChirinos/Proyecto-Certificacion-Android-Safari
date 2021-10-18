2249b3c1317259668441744c08334d80f26e323c
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-10-18 07:52:11 -0400

    Profile image on feed

--- app/src/main/java/edu/upb/tumejorviaje/data/TempDataSource.kt
+++ app/src/main/java/edu/upb/tumejorviaje/data/TempDataSource.kt
@@ -9,7 +9,7 @@ object TempDataSource {
             title = "De camino a mi maestría en China",
             shortDescription = "hace unos meses me llego la carta de aceptación en una de las mejores universidades...",
             postUrl = "https://sociable.co/wp-content/uploads/2019/05/guiyang-china-skyline-750x450.jpg",
-            profileUrl = "",
+            profileUrl = "https://cdn-icons-png.flaticon.com/512/2922/2922561.png",
             longDescription = "El Lorem Ipsum fue concebido como un texto de relleno, formateado de una cierta manera para permitir la presentación de elementos gráficos en documentos, sin necesidad de una copia formal. El uso de Lorem Ipsum permite a los diseñadores reunir los diseños y la forma del contenido antes de que el contenido se haya creado, dando al diseño y al proceso de producción más libertad.\n" +
                     "\n" +
                     "Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual. Sin embargo, las referencias a la frase \"Lorem Ipsum\" se pueden encontrar en la Edición de la Biblioteca Clásica Loeb de 1914 del De Finibus en las secciones 32 y 33. Fue en esta edición del De Finibus en la que H. Rackman tradujo el texto. El siguiente fragmento se selecciona de la sección 32:"
@@ -19,7 +19,7 @@ object TempDataSource {
             title = "Especialidad Oftalmologia en Brasil",
             shortDescription = "Despues de acabar la carrera de medicina mi sueño siempre fue ser oftalmologo...",
             postUrl = "https://static.hosteltur.com/app/public/uploads/img/articles/2014/12/01/L_5c1a3eae0668b_sao_paulo.jpg",
-            profileUrl = "",
+            profileUrl = "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
             longDescription = "El Lorem Ipsum fue concebido como un texto de relleno, formateado de una cierta manera para permitir la presentación de elementos gráficos en documentos, sin necesidad de una copia formal. El uso de Lorem Ipsum permite a los diseñadores reunir los diseños y la forma del contenido antes de que el contenido se haya creado, dando al diseño y al proceso de producción más libertad.\n" +
                     "\n" +
                     "Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual. Sin embargo, las referencias a la frase \"Lorem Ipsum\" se pueden encontrar en la Edición de la Biblioteca Clásica Loeb de 1914 del De Finibus en las secciones 32 y 33. Fue en esta edición del De Finibus en la que H. Rackman tradujo el texto. El siguiente fragmento se selecciona de la sección 32:"
@@ -29,7 +29,7 @@ object TempDataSource {
             title = "Vacaciones en Europa",
             shortDescription = "Mi sueño siempre fue conocer europa, conoci alrededor de quince países en tres semanas...",
             postUrl = "https://www.viajarafrancia.com/wp-content/uploads/2016/04/Paris-760x500.jpg",
-            profileUrl = "",
+            profileUrl = "https://cdn-icons-png.flaticon.com/512/3220/3220315.png",
             longDescription = "El Lorem Ipsum fue concebido como un texto de relleno, formateado de una cierta manera para permitir la presentación de elementos gráficos en documentos, sin necesidad de una copia formal. El uso de Lorem Ipsum permite a los diseñadores reunir los diseños y la forma del contenido antes de que el contenido se haya creado, dando al diseño y al proceso de producción más libertad.\n" +
                     "\n" +
                     "Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual. Sin embargo, las referencias a la frase \"Lorem Ipsum\" se pueden encontrar en la Edición de la Biblioteca Clásica Loeb de 1914 del De Finibus en las secciones 32 y 33. Fue en esta edición del De Finibus en la que H. Rackman tradujo el texto. El siguiente fragmento se selecciona de la sección 32:"
@@ -39,7 +39,7 @@ object TempDataSource {
             title = "Tours de 15añeras",
             shortDescription = "El mejor regalo de quince años fue irme de crucero con mis mejores amigas...",
             postUrl = "https://static.eldeber.com.bo//Files/Original/sites/eldeber/img/2019/02/22/quinces.jpg",
-            profileUrl = "",
+            profileUrl = "https://cdn-icons-png.flaticon.com/512/2922/2922656.png",
             longDescription = "El Lorem Ipsum fue concebido como un texto de relleno, formateado de una cierta manera para permitir la presentación de elementos gráficos en documentos, sin necesidad de una copia formal. El uso de Lorem Ipsum permite a los diseñadores reunir los diseños y la forma del contenido antes de que el contenido se haya creado, dando al diseño y al proceso de producción más libertad.\n" +
                     "\n" +
                     "Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual. Sin embargo, las referencias a la frase \"Lorem Ipsum\" se pueden encontrar en la Edición de la Biblioteca Clásica Loeb de 1914 del De Finibus en las secciones 32 y 33. Fue en esta edición del De Finibus en la que H. Rackman tradujo el texto. El siguiente fragmento se selecciona de la sección 32:"
@@ -49,7 +49,7 @@ object TempDataSource {
             title = "Trabajando en Estados Unidos",
             shortDescription = "hace un mes recibi una oferta de pasante en una de las compañias más grandes de...",
             postUrl = "https://www.tendenciashoy.com/wp-content/uploads/2020/07/times-square-foto-anthony-rosset-unsplash.jpeg",
-            profileUrl = "",
+            profileUrl = "https://cdn-icons-png.flaticon.com/512/2922/2922688.png",
             longDescription = "El Lorem Ipsum fue concebido como un texto de relleno, formateado de una cierta manera para permitir la presentación de elementos gráficos en documentos, sin necesidad de una copia formal. El uso de Lorem Ipsum permite a los diseñadores reunir los diseños y la forma del contenido antes de que el contenido se haya creado, dando al diseño y al proceso de producción más libertad.\n" +
                     "\n" +
                     "Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual. Sin embargo, las referencias a la frase \"Lorem Ipsum\" se pueden encontrar en la Edición de la Biblioteca Clásica Loeb de 1914 del De Finibus en las secciones 32 y 33. Fue en esta edición del De Finibus en la que H. Rackman tradujo el texto. El siguiente fragmento se selecciona de la sección 32:"
