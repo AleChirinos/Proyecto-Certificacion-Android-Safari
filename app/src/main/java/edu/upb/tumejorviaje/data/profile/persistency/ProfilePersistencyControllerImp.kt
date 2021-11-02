23bdf1431c6ab3a344453e815bb38cf00de75633
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-11-02 12:44:11 -0400

    Fix profile url

--- app/src/main/java/edu/upb/tumejorviaje/data/profile/persistency/ProfilePersistencyControllerImp.kt
+++ app/src/main/java/edu/upb/tumejorviaje/data/profile/persistency/ProfilePersistencyControllerImp.kt
@@ -4,7 +4,7 @@ import edu.upb.tumejorviaje.model.Post
 import edu.upb.tumejorviaje.model.User
 
 class ProfilePersistencyControllerImp : ProfilePersistencyController{
-    private var user: User?= User("dt.valdivia","Soy auditor financiero, con un master en big data y otro en accounting management", "https://vevico.files.wordpress.com/2019/08/tachuri-siete-colores.png?crop")
+    private var user: User?= User("dt.valdivia","Soy auditor financiero, con un master en big data y otro en accounting management", "https://i.imgur.com/RBZQYyP.png")
 
 
     //sin esto, el LocalDate no nos podra funcionar, ya que nos indica que requiere un API minimo de 26
